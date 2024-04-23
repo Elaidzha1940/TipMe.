ca405f368a917c2e18ae1ca0440d5d2fd83a6b6a
Elaidzha1940 <eli080293@gmail.com>
2024-04-24 01:24:38 +0300

    Added PriceCardView

--- TipMe/ContentView.swift
+++ TipMe/ContentView.swift
@@ -10,9 +10,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var bill: String = ""
+    
     var body: some View {
-        VStack {
-          
+        NavigationStack {
+            VStack {
+                PriceCardView()
+                
+                Text("Enter your total bill amount")
+                    .font(.system(size: 20, weight: .semibold, design: .default))
+
+                TextField("", text: $bill)
+                    .frame(width: 350, height: 50)
+                    .background(Color.secondary.opacity(0.3))
+                    .cornerRadius(20)
+            }
+            .navigationTitle("Tip Me")
         }
     }
 }
---
+++ TipMe/Views/PriceCardView.swift
@@ -0,0 +1,39 @@
+//  /*
+//
+//  Projcet: TipMe
+//  File: PriceCardView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.04.2024
+//
+//  */
+
+import SwiftUI
+
+struct PriceCardView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Group {
+                Text("Total per person: 800.00")
+                Text("Grand Total: : 300.00")
+            }
+            .font(.system(size: 20, weight: .bold, design: .default))
+           
+            Group {
+                Text("Bill: 150")
+                Text("Your Tip: 50 (20%)")
+                Text("Split by: 2")
+            }
+            .opacity(0.7)
+            .font(.system(size: 16, weight: .semibold, design: .default))
+
+        }
+        .frame(width: 350, height: 175)
+        .background(Color("bg"))
+        .cornerRadius(20)
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    PriceCardView()
+}
