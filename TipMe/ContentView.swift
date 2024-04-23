dbf53b51d8f84cb0c1a0a13e24b89619d976e6b2
Elaidzha1940 <eli080293@gmail.com>
2024-04-23 18:45:05 +0300

    TipMe

---
+++ TipMe/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: TipMe
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date 23.04.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipMe/TipMeApp.swift
@@ -0,0 +1,20 @@
+//  /*
+//
+//  Project: TipMe
+//  File: TipMeApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date 23.04.2024
+//
+//  */
+
+
+import SwiftUI
+
+@main
+struct TipMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
